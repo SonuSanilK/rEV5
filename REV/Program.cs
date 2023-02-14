ee305c30f3a88b5665067ddda64c9c6345c8d71d
sonu.k <sonu.k@SONUSANILK>
2023-02-14 16:48:05 +0530

    Add project files.

---
+++ REV/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter the name");
+string name = Console.ReadLine();
+string rev = string.Empty;
+for (int i = name.Length - 1; i >= 0; i--)
+{
+    rev += name[i];
+}
+Console.WriteLine($"Reverse is {rev}");
